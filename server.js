76851b4f8f1d096b940eeef9e6c31c95a9fcb841
sayon1998 <sayonchakraborty1998@gmail.com>
2020-12-26 20:57:46 +0530

    third commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotEnv = require("dotenv");
-const port = Process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 // Routes
 const allPostRoute = require("./Routers/post");
