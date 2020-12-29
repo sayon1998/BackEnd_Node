b9455be303ffafc9c1eac90a901d07a53b25e908
sayon1998 <sayonchakraborty1998@gmail.com>
2020-12-29 21:30:47 +0530

    CORS Set

--- server.js
+++ server.js
@@ -28,6 +28,16 @@ app.use(
     useTempFiles: true,
   })
 );
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 // Router Middleware
 app.use("/api", allPostRoute);
 app.use("/api", allGetRoute);
